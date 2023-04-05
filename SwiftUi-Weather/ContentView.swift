06957bad924ba8b1a97ab9e486ab6b12d4fe973a
tauantcamargo <lukyhenson@gmail.com>
2023-04-05 02:09:41 -0300

    fix: fixing the oldest way to use gradient

--- SwiftUi-Weather/ContentView.swift
+++ SwiftUi-Weather/ContentView.swift
@@ -79,10 +79,13 @@ struct WeatherDayView: View {
 struct BackgroundView: View {
     var isNight: Bool
     var body: some View {
-        LinearGradient(gradient: Gradient(colors: [isNight ? .black : .blue, isNight ? .gray : Color("lightBlue")]),
-                       startPoint: .topLeading,
-                       endPoint: .bottomTrailing)
-        .ignoresSafeArea()
+//        LinearGradient(gradient: Gradient(colors: [isNight ? .black : .blue, isNight ? .gray : Color("lightBlue")]),
+//                       startPoint: .topLeading,
+//                       endPoint: .bottomTrailing)
+//        .ignoresSafeArea()
+        ContainerRelativeShape()
+            .fill(isNight ? Color.black.gradient : Color.blue.gradient)
+            .ignoresSafeArea()
     }
 }
 
